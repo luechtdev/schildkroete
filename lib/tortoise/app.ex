14a12a14fd24e87b636e1144d61eb502d9111f96
Jannes Luecht <jannes.luecht@droniq.de>
2024-05-22 15:01:17 +0200

    Fix Application startup of websocket integration

--- lib/tortoise/app.ex
+++ lib/tortoise/app.ex
@@ -2,20 +2,31 @@ defmodule Tortoise.App do
   @moduledoc false
 
   use Application
+  alias Tortoise.Transport.Websocket
 
   @impl true
-  def start(_type, _args) do
+  def start(type, _args) do
     # read configuration and start connections
     # start with client_id, and handler from config
+
+    # Start websocket app
+    Application.start(Websocket.Application, type)
 
     children = [
       {Registry, [keys: :unique, name: Tortoise.Registry]},
       {Registry, [keys: :duplicate, name: Tortoise.Events]},
-      {Tortoise.Transport.Websocket.Application, []},
       {Tortoise.Supervisor, [strategy: :one_for_one]}
     ]
 
     opts = [strategy: :one_for_one, name: Tortoise]
     Supervisor.start_link(children, opts)
   end
+
+  @impl true
+  def stop(_state) do
+    # Propagate stop message to websocket app
+    Application.stop(Websocket.Application)
+    :ok
+  end
+
 end
